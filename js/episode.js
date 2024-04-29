072018f14ed392352b1d1ce22658aa21bf78715c
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:32:13 +0600

    improve

--- js/episode.js
+++ js/episode.js
@@ -1,4 +1,4 @@
 // API configurations
 const PROXY_API_BASE_URL = "https://proxy.techzbots1.workers.dev/?u="; // Base URL for the proxy API used to fetch data from external servers
-const ANIME_API_BASE_PATH = "/anime/"; // Base path for the anime-related API endpoints
-const EPISODE_API_BASE_PATH = "/episode/"; // Base path for the episode-related API endpoints
+const ANIME_API_BASE_PATH = "/anime/"; // Base path for anime-related API endpoints
+const EPISODE_API_BASE_PATH = "/episode/"; // Base path for episode-related API endpoints
